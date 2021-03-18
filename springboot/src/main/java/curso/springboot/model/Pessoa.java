d184e640b536db6fbd0e4d62d18475a3a2af922d
Marcelo-F <54288817+Marcelo-F@users.noreply.github.com>
2021-03-18 18:58:41 -0300

    Atualização 18/03/2021

---
+++ springboot/src/main/java/curso/springboot/model/Pessoa.java
@@ -0,0 +1,70 @@
+package curso.springboot.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javassist.SerialVersionUID;
+
+@Entity
+public class Pessoa implements Serializable{
+	
+	private static final long SerialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String nome;
+	
+	private String sobrenome;
+	
+	private int idade;
+	
+	@Autowired
+	@OneToMany(mappedBy = "pessoa")
+	private List<Telefone> telefones;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	
+	
+	
+	
+}
